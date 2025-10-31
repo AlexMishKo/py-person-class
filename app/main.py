78a5d3cbca37c104ec1591cfe59c83d2fad934f0
Oleksii Mishchenko <alex3hard@gmail.com>
2025-10-31 15:10:27 +0200

    removed comprahantion

--- app/main.py
+++ app/main.py
@@ -1,27 +1,22 @@
 class Person:
-    people: dict[str, "Person"] = {}
+    people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[self.name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list[Person]:
 
-    if not people:
-        return []
-
-    [
+    for person_data in people:
         Person(name=person_data["name"], age=person_data["age"])
-        for person_data in people
-    ]
 
     for person_data in people:
         name = person_data["name"]
         current_instance = Person.people[name]
         partner_name = person_data.get("wife") or person_data.get("husband")
-        if partner_name is not None:
+        if partner_name:
             partner_instance = Person.people.get(partner_name)
             if "wife" in person_data:
                 current_instance.wife = partner_instance
